9f1049f0f855f0c79f48474d8d109129d6ee3912
Ayman Laouini Khalifa <ayman.laouiniv@gmail.com>
2026-09-22 14:05:19 +0200

    primeros ejercicios PHP relacionado con Strings

--- Ejercicios Strings/ej3_strings.php
+++ Ejercicios Strings/ej3_strings.php
@@ -4,9 +4,9 @@
 <?php
  $email = "alberto.garcia@educa.madrid.org";
 
- print("Email: " . $email);
+ print("Email: " . $email . "<br>");
 
- $Completo = explode("@", $email);
+ $completo = explode("@", $email);
 
      $nombreUsuario = $completo[0];
      $Dominio = $completo[1];
@@ -19,9 +19,21 @@
    $Dominio = explode("." ,  $completo[1]);
 
       $Organizacion = $Dominio[0];
+      $municipio = $Dominio[1];
       $Extension = $Dominio[2];
 
+      $usuarioCaracteres = strlen($nombreUsuario[0]) + strlen($nombreUsuario[1]);
+      $dominioCaracteres = strlen($Dominio[0]) + strlen($Dominio[1]) + strlen($Dominio[2]) ;
+
   print("Usuario: " . $nombre . "." . $primerApellido ."<br>");
+  print("Dominio: " . $Organizacion . "." . $municipio . "." . $Extension . "<br>");
+  print("Organizacion: " . $Organizacion. "<br>");
+  print("Extension: " . $Extension. "<br>" . "<br>");
+
+
+
+  print("El usuario contiene: " . $usuarioCaracteres. "<br>");
+  print("El dominio contiene: " . $dominioCaracteres);
 
 ?>
 </BODY>
